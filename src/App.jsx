4fb3d57bae6f12358a8448458c375e09e9d556cf
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-18 20:06:02 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegistrationPage from "./components/registration"
+import ProfileDetailsPage from "./components/details"
+import ProfileUpdatePage from './components/profileUpdate'
+
+
+const App = () => {
+  return (
+    <div>
+   
+    </div>
+  );
+};
+
+export default App;
