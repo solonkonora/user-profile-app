82e81acb5e8ca66acd8c3ce4ad83cf7e97fa6e3b
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-18 23:08:14 -0800

    add jsx and css code for the details page

--- src/components/details/index.jsx
+++ src/components/details/index.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import styles from './details.module.css';
 
+// eslint-disable-next-line react/prop-types
 const DetailsPage = ({ profile }) => {
   return (
     <div>
