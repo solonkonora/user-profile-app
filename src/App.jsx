92be9420b3eb9fb495d5f4c22f5db259838ad310
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-18 23:28:31 -0800

    add jsx and css code for the app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import RegistrationPage from './components/registration';
+import DetailsPage from './components/details';
+import ProfileUpdatePage from './components/profileUpdate';
+// import styles from '.App.css'
 
-function App() {
+const App = () => {
+  const [profile, setProfile] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  });
+
+  const handleProfileUpdate = (updatedProfile) => {
+    setProfile(updatedProfile);
+  };
+
   return (
     <Router>
       <div>
@@ -13,25 +30,25 @@ function App() {
               <Link to="/details">Details</Link>
             </li>
             <li>
-              <Link to="/profileUpdate">Profile Update</Link>
+              <Link to="/profile-update">Profile Update</Link>
             </li>
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/registration"> 
-          <registration />
+          <Route path="/registration">
+            <RegistrationPage />
           </Route>
           <Route path="/details">
-            <details />
+            <DetailsPage profile={profile} />
           </Route>
           <Route path="/profile-update">
-            <profileUpdate />
+            <ProfileUpdatePage profile={profile} onUpdate={handleProfileUpdate} />
           </Route>
         </Switch>
       </div>
     </Router>
   );
+};
 
-}
 export default App;
