14ebb05a3d90545e5a1288603e137175009ac6f5
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-02-09 00:46:36 -0800

    add profile image to registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import './App.module.css';
+// import './App.module.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistrationPage from "./pages/registration";
 import DetailsPage from "./pages/details";
 import ProfileUpdatePage from "./pages/profileUpdate";
-// import NavBar from "./components/nav-bar/nav-bar";
 
 const App = () => {
   // inside the app Component, the usestate hook is used to declear a state variable called profile and its setter function, profile is an object with an initial value of empty strings
--- src/pages/registration/index.jsx
+++ src/pages/registration/index.jsx
@@ -1,7 +1,7 @@
-// eslint-disable-next-line no-unused-vars
+//eslint-disable-next-line no-unused-vars
 import React from "react";
-import { useFormik } from "formik";
 import "./registration.module.css";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 
@@ -25,7 +25,6 @@ function RegistrationForm() {
         .matches(/^[6][7][5-9]\d{6}$/, "Invalid phone number")
         .required("Phone Number is required"),
     }),
-
     onSubmit: (values) => {
       // Handle form submission
       console.log(values);
@@ -46,8 +45,57 @@ function RegistrationForm() {
     },
   });
 
+  const handleFileUpload = () => {
+    const fileInput = document.querySelector("#myfile");
+    const previewImage = document.querySelector("#previewImage");
+
+    fileInput.addEventListener("input", (e) => {
+      const file = e.target.files[0];
+      const image_url = URL.createObjectURL(file);
+      previewImage.src = image_url;
+    });
+  };
+
+  const handleEvent = () => {
+    // Access the file input element
+    const fileInput = document.querySelector("#myfile");
+
+    // Check if a file has been selected
+    if (fileInput && fileInput.files && fileInput.files.length > 0) {
+      const selectedFile = fileInput.files[0];
+
+      // Perform any desired operations with the selected file
+      console.log("Selected File:", selectedFile);
+
+      // Reset the file input value
+      fileInput.value = null;
+    } else {
+      console.log("No file selected.");
+    }
+  };
+
   return (
     <>
+      <input type="file" id="myfile" />
+      <img
+        id="previewImage"
+        src="https://media.istockphoto.com/id/501338715/photo/businesswoman-icon.jpg?s=612x612&w=0&k=20&c=t5vKOMCf_Kp8UKhQ_D6hLHDva0_MkLz03yZ6-9xqB2g="
+        alt="Preview"
+        onClick={handleEvent}
+        style={{
+          maxWidth: "150px",
+          maxHeight: "150px",
+          margin: "auto",
+          alignSelf: "center",
+          justifyContent: "center",
+        }}
+      />
+      <br />
+      <button onClick={handleFileUpload} id="uploadButton">
+        Upload
+      </button>
+      <br />
+
       <h1 className="heading">Registration</h1>
       <form onSubmit={formik.handleSubmit}>
         <div>
@@ -61,9 +109,7 @@ function RegistrationForm() {
             value={formik.values.firstName}
           />
           {formik.touched.firstName && formik.errors.firstName && (
-            <div style = { {color:"red"} }>
-              {formik.errors.firstName}
-            </div>
+            <div style={{ color: "red" }}>{formik.errors.firstName}</div>
           )}
         </div>
 
@@ -78,9 +124,7 @@ function RegistrationForm() {
             value={formik.values.lastName}
           />
           {formik.touched.lastName && formik.errors.lastName && (
-            <div style = { {color:"red"} }>
-              {formik.errors.lastName}
-            </div>
+            <div style={{ color: "red" }}>{formik.errors.lastName}</div>
           )}
         </div>
 
@@ -95,9 +139,7 @@ function RegistrationForm() {
             value={formik.values.email}
           />
           {formik.touched.email && formik.errors.email && (
-            <div style = { {color:"red"} }>
-              {formik.errors.email}
-            </div>
+            <div style={{ color: "red" }}>{formik.errors.email}</div>
           )}
         </div>
 
@@ -127,3 +169,109 @@ function RegistrationForm() {
 }
 
 export default RegistrationForm;
+
+// // eslint-disable-next-line no-unused-vars
+// import React from "react";
+// import FormikValidation from "../../components/nav-bar/formik-validation";
+// import "./registration.module.css";
+// import { useNavigate } from "react-router-dom";
+
+// function RegistrationForm() {
+//   const navigate = useNavigate();
+
+// {/* <FormikValidation onSubmit={formik.handleSubmit}/> */}
+
+// const handleSubmit = (values) => {
+//   //handle form submission
+//   console.log(values);
+
+//     // Store the form data in localStorage
+//     const formData = {
+//       firstName: values.firstName,
+//       lastName: values.lastName,
+//       email: values.email,
+//       phoneNumber: values.phoneNumber,
+//     };
+//     localStorage.setItem("formData", JSON.stringify(formData));
+
+//     // Reset the form fields
+//     formik.resetForm();
+
+//     navigate("details");
+//   };
+
+//   return (
+//     <>
+//       <h1 className="heading">Registration</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor="firstName">First Name:</label>
+//           <input
+//             type="text"
+//             id="firstName"
+//             name="firstName"
+//             onChange={formik.handleChange}
+//             onBlur={formik.handleBlur}
+//             value={formik.values.firstName}
+//           />
+//           {formik.touched.firstName && formik.errors.firstName && (
+//             <div style={{ color: "red" }}>{formik.errors.firstName}</div>
+//           )}
+//         </div>
+
+//         <div>
+//           <label htmlFor="lastName">Last Name:</label>
+//           <input
+//             type="text"
+//             id="lastName"
+//             name="lastName"
+//             onChange={formik.handleChange}
+//             onBlur={formik.handleBlur}
+//             value={formik.values.lastName}
+//           />
+//           {formik.touched.lastName && formik.errors.lastName && (
+//             <div style={{ color: "red" }}>{formik.errors.lastName}</div>
+//           )}
+//         </div>
+
+//         <div>
+//           <label htmlFor="email">Email:</label>
+//           <input
+//             type="email"
+//             id="email"
+//             name="email"
+//             onChange={formik.handleChange}
+//             onBlur={formik.handleBlur}
+//             value={formik.values.email}
+//           />
+//           {formik.touched.email && formik.errors.email && (
+//             <div style={{ color: "red" }}>{formik.errors.email}</div>
+//           )}
+//         </div>
+
+//         <div>
+//           <label htmlFor="phoneNumber">Phone Number:</label>
+//           <input
+//             type="text"
+//             id="phoneNumber"
+//             name="phoneNumber"
+//             onChange={formik.handleChange}
+//             onBlur={formik.handleBlur}
+//             value={formik.values.phoneNumber}
+//           />
+//           {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+//             <div style={{ color: "red", marginTop: "0.5rem" }}>
+//               {formik.errors.phoneNumber}
+//             </div>
+//           )}
+//         </div>
+
+//         <button type="submit" className="button1">
+//           Create Profile
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default RegistrationForm;
